b3d0c5d58c3bd3da67cff67619bc3082098f3dd9
AKIRA NAGANO <poruru.code@gmail.com>
2025-11-21 18:53:25 +0900

    Fix build: use Vite bundling instead of manual file copy

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    clearScreen: false,
+    server: {
+        strictPort: true,
+        port: 5173,
+    },
+    envPrefix: ['VITE_', 'TAURI_'],
+    build: {
+        target: ['es2021', 'chrome100', 'safari13'],
+        minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+        sourcemap: !!process.env.TAURI_DEBUG,
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: './index.html'
+            }
+        }
+    }
+});
